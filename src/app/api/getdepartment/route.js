4cfd1235e2f5ea39a07d3defda192ffa0aa95b52
bhavesh-bari <bhaveshbari0402@gmail.com>
2026-06-19 15:10:05 +0530

    departemts all

--- src/app/api/getdepartment/route.js
+++ src/app/api/getdepartment/route.js
@@ -2,21 +2,19 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Class from "@/models/Class";
 
-function getAcademicYear(date = new Date()) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    return month >= 5
-        ? `${year}-${String(year + 1).slice(2)}`
-        : `${year - 1}-${String(year).slice(2)}`;
-}
+// function getAcademicYear(date = new Date()) {
+//     const year = date.getFullYear();
+//     const month = date.getMonth();
+//     return month >= 5
+//         ? `${year}-${String(year + 1).slice(2)}`
+//         : `${year - 1}-${String(year).slice(2)}`;
+// }
 
 export async function GET(req) {
     try {
         await connectDB();
-        const academicYear = getAcademicYear();
-        const departments = await Class.distinct("department", {
-            academicYear,
-        });
+        // const academicYear = getAcademicYear();
+        const departments = await Class.distinct("department");
 
         return NextResponse.json(
             {
